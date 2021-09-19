4896e6556848ba1fce6aacfc0860bd2b784c5c22
öykü örs <oyku.ors@ceomed.net>
2021-09-19 14:53:13 +0300

    HttpClientModule was added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
@@ -28,7 +29,8 @@ import { AlertifyService } from './services/alertify.service';
   ],
   imports: [        // module
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [
     AlertifyService
